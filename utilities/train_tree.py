44573789aad4ed9e1552118057b81474dca07f80
Oliver Keller <oliver-keller@bluewin.ch>
2024-06-04 08:21:12 +0200

    inserted docstring, simplified code

--- utilities/train_tree.py
+++ utilities/train_tree.py
@@ -6,10 +6,26 @@ from sklearn import tree
 from sklearn.tree import export_text
 import copy
 
-from plot import plot_tree, plot_and_save_spyder_plots
+from utilities.plot import plot_tree, plot_and_save_spyder_plots
 
 #%%
 def train_tree_and_reorder(df_input_with_cluster,short_names,figure_folder,colors,plot_all_spyders,tree_size=(15,10)):
+    """
+    Trains a decision tree classifier using the input data and returns the reordered dataframe, nodes, and choices.
+    Reordering means that the cluster labels are reordered based on the decision tree classifier.
+
+    Parameters:
+        df_input_with_cluster (DataFrame): The input dataframe with cluster information.
+        short_names (list): The list of short names for the features.
+        figure_folder (str): The folder path to save the generated figures.
+        colors (list): The list of colors for plotting.
+        plot_all_spyders (bool): Flag indicating whether to plot all spyder plots.
+        tree_size (tuple, optional): The size of the decision tree plot. Defaults to (15, 10).
+
+    Returns:
+        tuple: A tuple containing the reordered dataframe, nodes, and choices.
+    """
+
     n_cl = df_input_with_cluster["initial cluster"].max() + 1
     categories = df_input_with_cluster.columns.to_list() 
     categories.remove("initial cluster")
@@ -18,18 +34,13 @@ def train_tree_and_reorder(df_input_with_cluster,short_names,figure_folder,color
 
     target_tree = df_input_with_cluster["initial cluster"]
 
-
-
-    n_leafnodes = [i for i in range(3,20)]
-    score = [0]*len(n_leafnodes)
-    i_score = 0
+    n_leafnodes = list(range(3,20))
+    score = []
 
     for n_leafs in n_leafnodes:
         interpretation_tree = tree.DecisionTreeClassifier(max_leaf_nodes=n_leafs,criterion="entropy")
         interpretation_tree.fit(df_input_with_cluster[categories],target_tree)
-        score[i_score] = interpretation_tree.score(df_input_with_cluster[categories],target_tree)
-        i_score += 1
-
+        score.append(interpretation_tree.score(df_input_with_cluster[categories],target_tree))
 
     plt.figure()
     plt.plot(n_leafnodes,score)
@@ -57,12 +68,10 @@ def train_tree_and_reorder(df_input_with_cluster,short_names,figure_folder,color
     plt.savefig(figure_folder+"/nice_tree.pdf")
 
     if plot_all_spyders:
-        plot_and_save_spyder_plots(interpretation_tree,categories, \
-                                df_input_with_cluster,short_names,figure_folder,colors)
+        plot_and_save_spyder_plots(interpretation_tree,categories, df_input_with_cluster,short_names,figure_folder,colors)
     
 
 
     return df_input_with_cluster, nodes, choices
-#%%
-# df_input_with_cluster.to_csv(folder+"df_input_with_cluster_tree.csv", sep = ";")
-# %%
+
+
