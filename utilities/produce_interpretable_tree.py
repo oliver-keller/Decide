10b8126e847c7aa93c63220be5b2b27f9f303e8b
Oliver Keller <oliver-keller@bluewin.ch>
2024-06-04 08:20:08 +0200

    added code to create an outoutfolder if not given as input or already existing, commented out  print statements

--- utilities/produce_interpretable_tree.py
+++ utilities/produce_interpretable_tree.py
@@ -1,17 +1,23 @@
-from cluster import cluster
-from train_tree import train_tree_and_reorder
+from utilities.cluster import cluster
+from utilities.train_tree import train_tree_and_reorder
 import copy
 import numpy as np
+import os
 from numpy.linalg import eig
-default_colors = ["b","c","k","g","m","r","y","tab:blue","tab:brown","tab:orange"\
-                  ,"tab:pink","tab:gree","tab:gray"]
-def produce_interpretable_tree(df_input, short_names,n_cl, \
-                               figure_folder="figures",tree_size=(15,10),colors=default_colors,plot_all_spyders = True):
-    df_input_with_initial_cluster = cluster(df_input, short_names,n_cl,figure_folder)
 
-    df_input_with_final_cluster, nodes, choices = train_tree_and_reorder(df_input_with_initial_cluster,short_names, \
-                                                         figure_folder,tree_size=tree_size,colors=colors,\
-                                                        plot_all_spyders=plot_all_spyders)
+
+default_colors = ["b","c","k","g","m","r","y","tab:blue","tab:brown", "tab:orange", "tab:pink","tab:gree","tab:gray"]
+
+def produce_interpretable_tree(df_input, short_names, n_cl, figure_folder=None, tree_size=(15,10), colors=default_colors,plot_all_spyders = True):
+
+    if figure_folder is None:
+        figure_folder = "figures"
+        if not os.path.exists(figure_folder):
+            os.makedirs(figure_folder)
+
+    df_input_with_initial_cluster = cluster(df_input, short_names, n_cl, figure_folder)
+
+    df_input_with_final_cluster, nodes, choices = train_tree_and_reorder(df_input_with_initial_cluster, short_names, figure_folder, tree_size=tree_size, colors=colors, plot_all_spyders=plot_all_spyders)
     
     categories = df_input_with_final_cluster.columns.to_list() 
     categories.remove("initial cluster")
@@ -42,7 +48,7 @@ def produce_interpretable_tree(df_input, short_names,n_cl, \
     for k in nodes.keys():
         for i in range(len(nodes[k])):
             if nodes[k][i] != []:
-                print(nodes[k][i])
+                # print(nodes[k][i])
                 df_node = df_input_with_final_cluster.loc[df_input_with_final_cluster["cluster_final"].isin(nodes[k][i])]
 
                 ranges_node = {}
@@ -53,13 +59,13 @@ def produce_interpretable_tree(df_input, short_names,n_cl, \
                 #calculate effective dimensionality
                 #lambdas ,v = eig(df_node[ categories].corr())
                 lambdas ,v = eig(df_node[ categories].cov())
-                print(lambdas)
+                # print(lambdas)
                 sum_l = sum(lambdas)
                 intermediate_term = [(l/sum_l)**(-l/sum_l) for l in lambdas]
-                print(intermediate_term)
+                # print(intermediate_term)
                 decision_space["effective_dimensionality"][k][i] = multiplyList(intermediate_term)
 
-                print(ranges_node)
+                # print(ranges_node)
                     
                 decision_space["sum"][k][i] = sum(ranges_node[cat] for cat in categories) \
                     /sum(ranges_initial[cat] for cat in categories)
